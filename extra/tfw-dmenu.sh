1874ca3bca685a64b478578c5c6ee5fbe68a20f6
NRK <nrk@rape.lol>
2021-04-01 13:21:57 +0600

    initial commit

---
+++ extra/tfw-dmenu.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Description: A dmenu wrapper around tfw.
+#
+# Dependencies: tfw (obviously) - https://github.com/climech/tfw
+#               dmenu or rofi
+#
+# Shell: POSIX compliant
+# Author: NRK
